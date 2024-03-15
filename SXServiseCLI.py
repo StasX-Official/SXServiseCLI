ff087ad1e8dc88995421df4fc916665df897aed4
StasX <141828909+StasX-Official@users.noreply.github.com>
2024-03-15 19:19:21 +0200

    Update SXServiseCLI.py

--- SXServiseCLI.py
+++ SXServiseCLI.py
@@ -1,3 +1,8 @@
-from jsondataupg import app_name, version, app_id, com, author, description, license, api_enabled, api_path, logs_enabled, username, password, mail, ai_support, local_default_port, local_hosting_support, local_default_path
+from jsondataupg import app_name, version, app_id, com, author, description, license, api_enabled, api_path, logs_enabled, ai_support, local_default_port, local_hosting_support, local_default_path
+
+from System.Local.sxg.core import root_name, root_pass
+
+    
 from System.main import start_all
-start_all(app_name, version, app_id, com, author, description, license, api_enabled, api_path, logs_enabled, ai_support, local_default_port, local_hosting_support, local_default_path)
+start_all(app_name, version, app_id, com, author, description, license, api_enabled, api_path, logs_enabled, ai_support, local_default_port, local_hosting_support, local_default_path, root_name, root_pass)
+
