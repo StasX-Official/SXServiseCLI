6d20884bb8a20610646b707a6f5158d964b3bcbc
StasX <141828909+StasX-Official@users.noreply.github.com>
2024-05-17 19:29:35 +0300

    Update core.py

--- System/Local/sxg/core.py
+++ System/Local/sxg/core.py
@@ -1,6 +1,31 @@
-def core_main():
+
+def core_main(code, data):
     #Запуск програм
-    pass
+    if code == 0: #STOP_CORE
+        if data == 1:
+            from System.Local.sxg.cors.sxg_servises_core import core_sxservise_api
+            status = core_sxservise_api(4, 0, 0, 0)
+            print(status)
+    elif code == 1: #START_CORE
+        if data == 0:
+            from System.Local.sxg.cors.sxg_servises_core import core_sxservise_api
+            status = core_sxservise_api(3, 0, 0, 0)
+            print(status)
+    elif code == 3: #GET_CORE_INFO - STATUS
+        if data == 0: #GET_ALL_CORE_INFO
+            from System.Local.sxg.cors.sxg_servises_core import core_sxservise_api
+            status = core_sxservise_api(8, 0, 0, 0)
+            if status == False:
+                return False
+            elif status == True:
+                return True
+            else:
+                return "ERROR"
+        else:
+            return "Error data_st"
+    else:
+        return "Error to code_data."
+
 
 
 
@@ -28,3 +53,7 @@ def core_user_auth(user_name,user_passworld,user_mail):
     
 
 root_name, root_pass = core_init()
+
+#core_main(1,0) #Запуск ядра сервісу
+#servisecore_st = core_main(3,0) #Перевірка ядра сервісу
+#print(servisecore_st) 
