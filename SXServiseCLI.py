d5f3b541f918c9c1dedb8b1457be474cccf0534d
StasX <141828909+StasX-Official@users.noreply.github.com>
2024-07-14 19:38:51 +0300

    Update SXServiseCLI.py

--- SXServiseCLI.py
+++ SXServiseCLI.py
@@ -1,11 +1,5 @@
 from jsondataupg import user1mail, user1name, sxservisecliPLUSuser, app_name, version, app_id, com, author, description, license, api_enabled, api_path, logs_enabled, ai_support, local_default_port, local_hosting_support, local_default_path
-
-#ROOT_NAME
 root_name = "sxservisecli1"
-#ROOT_PASS
 root_pass = "sxservisecli1"
-
-#STARTING SXSERVISECLI
 from System.main import start_all
 start_all(user1mail, user1name, sxservisecliPLUSuser, app_name, version, app_id, com, author, description, license, api_enabled, api_path, logs_enabled, ai_support, local_default_port, local_hosting_support, local_default_path, root_name, root_pass)
-
