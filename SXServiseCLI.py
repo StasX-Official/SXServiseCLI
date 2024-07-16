511b4a1c026a5d2879bc915a804a81346a2b2134
StasX <141828909+StasX-Official@users.noreply.github.com>
2024-07-16 17:29:28 +0300

    Update SXServiseCLI.py
    Signed-off-by: StasX <141828909+StasX-Official@users.noreply.github.com>

--- SXServiseCLI.py
+++ SXServiseCLI.py
@@ -1,5 +1,7 @@
 from jsondataupg import user1mail, user1name, sxservisecliPLUSuser, app_name, version, app_id, com, author, description, license, api_enabled, api_path, logs_enabled, ai_support, local_default_port, local_hosting_support, local_default_path
 root_name = "sxservisecli1"
 root_pass = "sxservisecli1"
+from addons_system import addons_data_system
+from api_system import apidata
 from System.main import start_all
-start_all(user1mail, user1name, sxservisecliPLUSuser, app_name, version, app_id, com, author, description, license, api_enabled, api_path, logs_enabled, ai_support, local_default_port, local_hosting_support, local_default_path, root_name, root_pass)
+start_all(user1mail, user1name, sxservisecliPLUSuser, app_name, version, app_id, com, author, description, license, api_enabled, api_path, logs_enabled, ai_support, local_default_port, local_hosting_support, local_default_path, root_name, root_pass, addons_data_system, apidata)
