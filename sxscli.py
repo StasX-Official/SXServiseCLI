c59daa65c7591739e498f2d518de42fba064ae2f
Kozosvyst Stas (StasX) <141828909+StasX-Official@users.noreply.github.com>
2025-02-09 11:26:24 +0200

    Update sxscli.py
    Signed-off-by: Kozosvyst Stas (StasX) <141828909+StasX-Official@users.noreply.github.com>

--- sxscli.py
+++ sxscli.py
@@ -1,17 +1,40 @@
 from System.main import SXServiseCLI
 from System.commands_map import commands_map
-import argparse,os
+import argparse, os, sys, ctypes, json, winreg, platform
+
+def is_admin(): return ctypes.windll.shell32.IsUserAnAdmin() != 0 if sys.platform == "win32" else False
+
+def add_to_path():
+    p = os.path.dirname(os.path.abspath(__file__))
+    if sys.platform == "win32":
+        try:
+            with winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Environment", 0, winreg.KEY_SET_VALUE) as k:
+                v = winreg.QueryValueEx(k, "Path")[0]
+                if p not in v:
+                    winreg.SetValueEx(k, "Path", 0, winreg.REG_EXPAND_SZ, f"{v};{p}")
+                    os.system("taskkill /f /im explorer.exe && start explorer.exe")
+        except: pass
+    elif sys.platform == "linux":
+        try:
+            if data.get("add_to_path_linux_", False):
+                with open(os.path.expanduser("~/.bashrc"), "a") as bashrc: 
+                    bashrc.write(f'\nexport PATH="$PATH:{p}"\n')
+        except: pass
+    else:
+        pass
+
 try:
-    parser = argparse.ArgumentParser(
-        description="SXServiseCLI 2024 - is a powerful and versatile command-line tool that helps you run local services, generate JSON files, create QR codes, test APIs, SSH, FTP, and much more."
-    )
-    parser.add_argument('command', type=str, help='Command, ex.: ssh')
+    with open("System/configs/control.json", "r") as f:
+        data = json.load(f)
+    data.setdefault("add_to_path_linux_", False)
+    admin_rights = is_admin() if data.get("check_admin_rights_", False) else True
+    if data.get("add_to_path_windows_", False) and sys.platform == "win32" and admin_rights:
+        add_to_path()
+    elif sys.platform == "linux" and data.get("add_to_path_linux_", False):
+        add_to_path()
+    parser = argparse.ArgumentParser(description="SXServiseCLI 2024")
+    parser.add_argument('command', type=str, help='Command (e.g., help)')
     os.system('cls' if os.name == 'nt' else 'clear')
     args = parser.parse_args()
-
-    command_to_run = commands_map.get(args.command, "run")
-    SXServiseCLI(other=command_to_run)
-except:
-    SXServiseCLI("run")
-
-#python sxscli.py ***
+    SXServiseCLI(other=commands_map.get(args.command, "run"), admin_rights=admin_rights)
+except: SXServiseCLI("run", admin_rights=admin_rights)
